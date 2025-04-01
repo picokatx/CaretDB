917dd689c0435737f3ae2e0f7ed4218b9821d74d
picokatx <picokatx@gmail.com>
2025-04-01 08:47:02 +0800

    🚚 issl 2

--- SpyNet/app.vue
+++ SpyNet/app.vue
@@ -1,24 +1,12 @@
 <template>
-  <NuxtLoadingIndicator />
-  <Toolbar class="my-3 mx-1 p-3">
-    <template #start>
-      <div class="flex">
-        <Button icon="pi pi-home" class="spynet-nav" aria-label="Landing" @click="navigateTo('/');" text plain/>
-        <Button id="database-link" class="spynet-nav" label="Database" @click="navigateTo('/database');" text plain/>
-        <Button id="references-link" class="spynet-nav" label="References" @click="navigateTo('/references');" text plain />
-      </div>
-    </template>
-    <template #end>
-      <div class="flex">
-        <Button id="login-link" class="spynet-nav" label="Login" @click="navigateTo('/login');" text plain />
-      </div>
-    </template>
-  </Toolbar>
+  <SpyNetNavbar />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
 <script setup lang="ts">
+import SpyNetNavbar from './components/SpyNetNavbar.vue';
+
 // const goto_references = useState('goto_references', () => 0)
 useHead
   ({
---
+++ SpyNet/components/SpyNetNavbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLoadingIndicator />
+  <Toolbar class="my-3 mx-1 p-3">
+    <template #start>
+      <div class="flex">
+        <Button icon="pi pi-home" class="spynet-nav" aria-label="Landing" @click="navigateTo('/');" text
+          plain />
+        <Button id="database-link" class="spynet-nav" label="Database" @click="navigateTo('/database');" text
+          plain />
+        <Button id="references-link" class="spynet-nav" label="References" @click="navigateTo('/references');"
+          text plain />
+      </div>
+    </template>
+    <template #end>
+      <div class="flex">
+        <Button id="login-link" class="spynet-nav" label="Login" @click="navigateTo('/login');" text plain />
+      </div>
+    </template>
+  </Toolbar>
+</template>
