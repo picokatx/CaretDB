b7264076c2ec7e7da99b16f5399c904485bb0ac2
picokatx <picokatx@gmail.com>
2025-04-21 09:34:02 +0800

    🚀 pnpm create astro@latest

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue()],
+  adapter: vercel()
+});
