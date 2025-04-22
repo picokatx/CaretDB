4891792313e3b19cf52bed0e8ccf758764c2384b
picokatx <picokatx@gmail.com>
2025-04-22 09:05:55 +0800

    ⚡️home page

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,14 +2,16 @@ import type { APIRoute } from 'astro'
 import { sql } from '../../lib/mysql-connect'
 import type { QueryResult, FieldPacket } from 'mysql2';
 
-export const GET: APIRoute = async () => {
+export const POST: APIRoute = async ({ params, request }) => {
   const [query_result, field_packet] = await sql.query(`
     SELECT * FROM employee;
     `);
-  
+
   return new Response(
     JSON.stringify({
-      rows: query_result
+      rows: query_result,
+      request_params: params,
+      request_body: request
     }),
   )
 }
