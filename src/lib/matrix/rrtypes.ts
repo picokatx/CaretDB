34bae70d3f3841ed81de7eafb51b0a9cb223daf0
picokatx <picokatx@gmail.com>
2025-04-29 10:13:24 +0800

    🔥 removed callbacks, unreferenced types

--- src/lib/matrix/rrtypes.ts
+++ src/lib/matrix/rrtypes.ts
@@ -1,3 +1,5 @@
+// -------------------------------------------------------------------------------------------
+// this is like a super event type of our current event type ddl
 export enum EventType {
     DomContentLoaded,
     Load,
@@ -20,17 +22,16 @@ export type loadedEvent = {
 export type fullSnapshotEvent = {
     type: EventType.FullSnapshot;
     data: {
-        node: serializedNodeWithId;
+        node: serializedNodeWithId;  // foreign key of seralized node
         initialOffset: {
             top: number;
             left: number;
         };
     };
 };
-
 export type incrementalSnapshotEvent = {
     type: EventType.IncrementalSnapshot;
-    data: incrementalData;
+    data: incrementalData;  // foreign key of event
 };
 
 export type metaEvent = {
@@ -49,7 +50,7 @@ export type customEvent<T = unknown> = {
         payload: T;
     };
 };
-
+// basically our current event ddl but more
 export enum IncrementalSource {
     Mutation,
     MouseMove,
@@ -72,7 +73,12 @@ export enum IncrementalSource {
 
 export type mutationData = {
     source: IncrementalSource.Mutation;
-} & mutationCallbackParam;
+    texts: textMutation[];
+    attributes: attributeMutation[];
+    removes: removedNodeMutation[];
+    adds: addedNodeMutation[];
+    isAttachIframe?: true;
+};
 
 export type mousemoveData = {
     source:
@@ -84,52 +90,131 @@ export type mousemoveData = {
 
 export type mouseInteractionData = {
     source: IncrementalSource.MouseInteraction;
-} & mouseInteractionParam;
+    type: MouseInteractions;
+    id: number;
+    x?: number;
+    y?: number;
+    pointerType?: PointerTypes;
+};
 
 export type scrollData = {
     source: IncrementalSource.Scroll;
-} & scrollPosition;
+    id: number;
+    x: number;
+    y: number;
+};
 
 export type viewportResizeData = {
     source: IncrementalSource.ViewportResize;
-} & viewportResizeDimension;
+    width: number;
+    height: number;
+};
 
 export type inputData = {
     source: IncrementalSource.Input;
     id: number;
-} & inputValue;
+    text: string;
+    isChecked: boolean;
+    userTriggered?: boolean;
+};
 
 export type mediaInteractionData = {
     source: IncrementalSource.MediaInteraction;
-} & mediaInteractionParam;
+    type: MediaInteractions;
+    id: number;
+    currentTime?: number;
+    volume?: number;
+    muted?: boolean;
+    loop?: boolean;
+    playbackRate?: number;
+};
+export enum MediaInteractions {
+    Play,
+    Pause,
+    Seeked,
+    VolumeChange,
+    RateChange,
+}
+
 
 export type styleSheetRuleData = {
     source: IncrementalSource.StyleSheetRule;
-} & styleSheetRuleParam;
+    id?: number;
+    styleId?: number;
+    removes?: styleSheetDeleteRule[];
+    adds?: styleSheetAddRule[];
+    replace?: string;
+    replaceSync?: string;
+};
 
 export type styleDeclarationData = {
     source: IncrementalSource.StyleDeclaration;
-} & styleDeclarationParam;
+    id?: number;
+    styleId?: number;
+    index: number[];
+    set?: {
+        property: string;
+        value: string | null;
+        priority: string | undefined;
+    };
+    remove?: {
+        property: string;
+    };
+};
 
 export type canvasMutationData = {
     source: IncrementalSource.CanvasMutation;
-} & canvasMutationParam;
+    property: string;
+    args: Array<unknown>;
+    setter?: true;
+    id: number;
+    type: CanvasContext;
+    commands: canvasMutationCommand[]
+};
+export type canvasMutationCommand = {
+    property: string;
+    args: Array<unknown>;
+    setter?: true;
+};
 
 export type fontData = {
     source: IncrementalSource.Font;
-} & fontParam;
+    family: string;
+    fontSource: string;
+    buffer: boolean;
+    descriptors?: FontFaceDescriptors;
+};
 
 export type selectionData = {
     source: IncrementalSource.Selection;
-} & selectionParam;
+    ranges: Array<SelectionRange>;
+};
+export type SelectionRange = {
+    start: number;
+    startOffset: number;
+    end: number;
+    endOffset: number;
+};
 
 export type adoptedStyleSheetData = {
     source: IncrementalSource.AdoptedStyleSheet;
-} & adoptedStyleSheetParam;
+    // id indicates the node id of document or shadow DOMs' host element.
+    id: number;
+    // New CSSStyleSheets which have never appeared before.
+    styles?: {
+        styleId: number;
+        rules: styleSheetAddRule[];
+    }[];
+    // StyleSheet ids to be adopted.
+    styleIds: number[];
+};
 
 export type customElementData = {
     source: IncrementalSource.CustomElement;
-} & customElementParam;
+    define?: {
+        name: string;
+    };
+};
 
 export type incrementalData =
     | mutationData
@@ -163,79 +248,6 @@ export type eventWithTime = eventWithoutTime & {
 export type canvasEventWithTime = eventWithTime & {
     type: EventType.IncrementalSnapshot;
     data: canvasMutationData;
-};
-
-export type blockClass = string | RegExp;
-
-export type maskTextClass = string | RegExp;
-
-export type SamplingStrategy = Partial<{
-    /**
-     * false means not to record mouse/touch move events
-     * number is the throttle threshold of recording mouse/touch move
-     */
-    mousemove: boolean | number;
-    /**
-     * number is the throttle threshold of mouse/touch move callback
-     */
-    mousemoveCallback: number;
-    /**
-     * false means not to record mouse interaction events
-     * can also specify record some kinds of mouse interactions
-     */
-    mouseInteraction: boolean | Record<string, boolean | undefined>;
-    /**
-     * number is the throttle threshold of recording scroll
-     */
-    scroll: number;
-    /**
-     * number is the throttle threshold of recording media interactions
-     */
-    media: number;
-    /**
-     * 'all' will record all the input events
-     * 'last' will only record the last input value while input a sequence of chars
-     */
-    input: 'all' | 'last';
-    /**
-     * 'all' will record every single canvas call
-     * number between 1 and 60, will record an image snapshots in a web-worker a (maximum) number of times per second.
-     *                          Number only supported where [`OffscreenCanvas`](http://mdn.io/offscreencanvas) is supported.
-     */
-    canvas: 'all' | number;
-}>;
-
-export interface ICrossOriginIframeMirror {
-    getId(
-        iframe: HTMLIFrameElement,
-        remoteId: number,
-        parentToRemoteMap?: Map<number, number>,
-        remoteToParentMap?: Map<number, number>,
-    ): number;
-    getIds(iframe: HTMLIFrameElement, remoteId: number[]): number[];
-    getRemoteId(
-        iframe: HTMLIFrameElement,
-        parentId: number,
-        map?: Map<number, number>,
-    ): number;
-    getRemoteIds(iframe: HTMLIFrameElement, parentId: number[]): number[];
-    reset(iframe?: HTMLIFrameElement): void;
-}
-
-export type hooksParam = {
-    mutation?: mutationCallBack;
-    mousemove?: mousemoveCallBack;
-    mouseInteraction?: mouseInteractionCallBack;
-    scroll?: scrollCallback;
-    viewportResize?: viewportResizeCallback;
-    input?: inputCallback;
-    mediaInteaction?: mediaInteractionCallback;
-    styleSheetRule?: styleSheetRuleCallback;
-    styleDeclaration?: styleDeclarationCallback;
-    canvasMutation?: canvasMutationCallback;
-    font?: fontCallback;
-    selection?: selectionCallback;
-    customElement?: customElementCallback;
 };
 
 // https://dom.spec.whatwg.org/#interface-mutationrecord
@@ -299,16 +311,6 @@ export type mutationCallbackParam = {
     adds: addedNodeMutation[];
     isAttachIframe?: true;
 };
-
-export type mutationCallBack = (m: mutationCallbackParam) => void;
-
-export type mousemoveCallBack = (
-    p: mousePosition[],
-    source:
-        | IncrementalSource.MouseMove
-        | IncrementalSource.TouchMove
-        | IncrementalSource.Drag,
-) => void;
 
 export type mousePosition = {
     x: number;
@@ -381,24 +383,6 @@ export type CanvasArg =
     | null
     | CanvasArg[];
 
-type mouseInteractionParam = {
-    type: MouseInteractions;
-    id: number;
-    x?: number;
-    y?: number;
-    pointerType?: PointerTypes;
-};
-
-export type mouseInteractionCallBack = (d: mouseInteractionParam) => void;
-
-export type scrollPosition = {
-    id: number;
-    x: number;
-    y: number;
-};
-
-export type scrollCallback = (p: scrollPosition) => void;
-
 export type styleSheetAddRule = {
     rule: string;
     index?: number | number[];
@@ -407,75 +391,6 @@ export type styleSheetAddRule = {
 export type styleSheetDeleteRule = {
     index: number | number[];
 };
-
-export type styleSheetRuleParam = {
-    id?: number;
-    styleId?: number;
-    removes?: styleSheetDeleteRule[];
-    adds?: styleSheetAddRule[];
-    replace?: string;
-    replaceSync?: string;
-};
-
-export type styleSheetRuleCallback = (s: styleSheetRuleParam) => void;
-
-export type adoptedStyleSheetParam = {
-    // id indicates the node id of document or shadow DOMs' host element.
-    id: number;
-    // New CSSStyleSheets which have never appeared before.
-    styles?: {
-        styleId: number;
-        rules: styleSheetAddRule[];
-    }[];
-    // StyleSheet ids to be adopted.
-    styleIds: number[];
-};
-
-export type adoptedStyleSheetCallback = (a: adoptedStyleSheetParam) => void;
-
-export type styleDeclarationParam = {
-    id?: number;
-    styleId?: number;
-    index: number[];
-    set?: {
-        property: string;
-        value: string | null;
-        priority: string | undefined;
-    };
-    remove?: {
-        property: string;
-    };
-};
-
-export type styleDeclarationCallback = (s: styleDeclarationParam) => void;
-
-export type canvasMutationCommand = {
-    property: string;
-    args: Array<unknown>;
-    setter?: true;
-};
-
-export type canvasMutationParam =
-    | {
-        id: number;
-        type: CanvasContext;
-        commands: canvasMutationCommand[];
-    }
-    | ({
-        id: number;
-        type: CanvasContext;
-    } & canvasMutationCommand);
-
-export type canvasMutationWithType = {
-    type: CanvasContext;
-} & canvasMutationCommand;
-
-export type canvasMutationCallback = (p: canvasMutationParam) => void;
-
-export type canvasManagerMutationCallback = (
-    target: HTMLCanvasElement,
-    p: canvasMutationWithType,
-) => void;
 
 export type ImageBitmapDataURLWorkerParams = {
     id: number;
@@ -497,55 +412,6 @@ export type ImageBitmapDataURLWorkerResponse =
         height: number;
     };
 
-export type fontParam = {
-    family: string;
-    fontSource: string;
-    buffer: boolean;
-    descriptors?: FontFaceDescriptors;
-};
-
-export type fontCallback = (p: fontParam) => void;
-
-export type viewportResizeDimension = {
-    width: number;
-    height: number;
-};
-
-export type viewportResizeCallback = (d: viewportResizeDimension) => void;
-
-export type inputValue = {
-    text: string;
-    isChecked: boolean;
-
-    // `userTriggered` indicates if this event was triggered directly by user (userTriggered: true)
-    // or was triggered indirectly (userTriggered: false)
-    // Example of `userTriggered` in action:
-    // User clicks on radio element (userTriggered: true) which triggers the other radio element to change (userTriggered: false)
-    userTriggered?: boolean;
-};
-
-export type inputCallback = (v: inputValue & { id: number }) => void;
-
-export enum MediaInteractions {
-    Play,
-    Pause,
-    Seeked,
-    VolumeChange,
-    RateChange,
-}
-
-export type mediaInteractionParam = {
-    type: MediaInteractions;
-    id: number;
-    currentTime?: number;
-    volume?: number;
-    muted?: boolean;
-    loop?: boolean;
-    playbackRate?: number;
-};
-
-export type mediaInteractionCallback = (p: mediaInteractionParam) => void;
-
 export type DocumentDimension = {
     x: number;
     y: number;
@@ -554,27 +420,6 @@ export type DocumentDimension = {
     // scale value relative to the root iframe
     absoluteScale: number;
 };
-
-export type SelectionRange = {
-    start: number;
-    startOffset: number;
-    end: number;
-    endOffset: number;
-};
-
-export type selectionParam = {
-    ranges: Array<SelectionRange>;
-};
-
-export type selectionCallback = (p: selectionParam) => void;
-
-export type customElementParam = {
-    define?: {
-        name: string;
-    };
-};
-
-export type customElementCallback = (c: customElementParam) => void;
 
 export type throttleOptions = {
     leading?: boolean;
@@ -626,31 +471,6 @@ export enum ReplayerEvents {
     PlayBack = 'play-back',
     Destroy = 'destroy',
 }
-
-export type KeepIframeSrcFn = (src: string) => boolean;
-
-declare global {
-    interface Window {
-        FontFace: typeof FontFace;
-    }
-}
-
-export type IWindow = Window & typeof globalThis;
-
-export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
-
-export type GetTypedKeys<Obj extends object, ValueType> = TakeTypeHelper<
-    Obj,
-    ValueType
->[keyof TakeTypeHelper<Obj, ValueType>];
-export type TakeTypeHelper<Obj extends object, ValueType> = {
-    [K in keyof Obj]: Obj[K] extends ValueType ? K : never;
-};
-
-export type TakeTypedKeyValues<Obj extends object, Type> = Pick<
-    Obj,
-    TakeTypeHelper<Obj, Type>[keyof TakeTypeHelper<Obj, Type>]
->;
 
 export enum NodeType {
     Document,
@@ -753,33 +573,7 @@ export type serializedElementNodeWithId = Extract<
     Record<'type', NodeType.Element>
 >;
 
-export interface IMirror<TNode> {
-    getId(n: TNode | undefined | null): number;
-
-    getNode(id: number): TNode | null;
-
-    getIds(): number[];
-
-    getMeta(n: TNode): serializedNodeWithId | null;
-
-    removeNodeFromMap(n: TNode): void;
-
-    has(id: number): boolean;
-
-    hasNode(node: TNode): boolean;
-
-    add(n: TNode, meta: serializedNodeWithId): void;
-
-    replace(id: number, n: TNode): void;
-
-    reset(): void;
-}
-
 export type DataURLOptions = Partial<{
     type: string;
     quality: number;
 }>;
-
-// Types for @rrweb/packer
-export type PackFn = (event: eventWithTime) => string;
-export type UnpackFn = (raw: string) => eventWithTime;
